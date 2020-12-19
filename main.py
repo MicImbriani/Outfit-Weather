a28defc89c54d638ebf5bbdf277a667d20045b3f
MicImbriani <imbr.mic@gmail.com>
2020-12-19 11:02:52 +0000

    added confusion matrix

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import network
 import dataset
 import torch.nn.functional as F
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 import torch.optim as optim
 
 
 
-# Print raw results
 def print_results(label, pred):
+    """Prints raw results.
+
+    Args:
+        label ([tensor]): [Label for each of the item predicted]
+        pred ([tensor]): [Prediction made for each item]
+    Returns:
+        None
+    """    
     print("The correct category is: ", label)
     print("The prediction values for each category are:\n", pred)
     print()
@@ -17,9 +27,14 @@ def print_results(label, pred):
 
     print(pred.argmax(dim=1))
 
+
+
 # Compute and print the total number of correct predictions
 def get_num_correct(preds, labels):
+
     return preds.argmax(dim=1).eq(labels).sum().item()
+
+
 
 # Compute and print all predictions 
 def get_all_preds(model, loader):
@@ -31,7 +46,24 @@ def get_all_preds(model, loader):
         all_preds = torch.cat((all_preds, preds), dim=0)
     
     return all_preds
-        
+
+
+
+def confusion_matrix(ts, tp):
+    conf_mat = confusion_matrix(ts.targets, tp.argmax(dim=1))
+
+    names = ('T-shirt/top', 'Trousers', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+    
+    plt.figure(figsize=(10,10))
+    plt.title('Confusion Matrix')
+    plt.labels('names')
+
+    table = plt.table(conf_mat,
+                    rowLabels=names,
+                    colLabels=names,
+                    loc='bottom')
+    plt.show()
+    return
 
 
 ############################################################################################################
@@ -54,7 +86,7 @@ b = image.unsqueeze(0)
 
 
 
-for epoch in range(5):
+for epoch in range(1):
     # Counters for visualisation of progress during training
     total_loss = 0
     total_correct = 0
@@ -84,10 +116,21 @@ for epoch in range(5):
 
 
         # Update the couters 
-        total_loss += loss.item()
+        total_loss = loss.item()
         total_correct += get_num_correct(preds, labels)
 
         # Print some information to keep track of the progress 
         print("epoch:", epoch, "total correct:", total_correct, "loss:", total_loss)
 
-print(total_correct/len(train_set))
+    print(total_correct/len(train_set))
+
+
+
+
+
+# Turn off gradient tracking locally to remove overhead of keeping track of gradients/creating graph
+with torch.no_grad():
+    prediction_loader = dataset.train_loader
+    train_preds = get_all_preds(network, prediction_loader)
+
+confusion_matrix(train_set, train_preds)
